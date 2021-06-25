ee6d7626268f20ea36adacaba538ebb9efe0ab0c
fatimakhaan <fatimaalikhan1167@gmail.com>
2021-06-25 16:24:55 +0500

    update
    update

---
+++ app.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+let btn = document.createElement("button");
+btn.innerHTML = "Click Me";
+document.body.appendChild(btn);
